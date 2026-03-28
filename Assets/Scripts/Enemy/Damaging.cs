0b94f9809dc8c2b26d3cb95a0ca8c577bb83375b
Arthur Zin <zarthy10111@gmail.com>
2026-03-28 11:57:22 -0400

    damage
    did damage

--- Assets/Scripts/Enemy/Damaging.cs
+++ Assets/Scripts/Enemy/Damaging.cs
@@ -2,16 +2,13 @@ using UnityEngine;
 
 public class Damaging : MonoBehaviour
 {
-    PlayerHp player;
+
     public float cd = 0f;
-    void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Logic").GetComponent<PlayerHp>();
-    }
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            PlayerHp player = other.GetComponent<PlayerHp>();
             if(cd > 2f){
                 cd = 0f;
                 player.takeDamage();
